a1c4fd1ee861d4bd7c342a61093e244cc0de086a
cbts96 <lexuanthang123@gmail.com>
2023-03-10 07:33:47 +0700

    Add header and auth feature

--- src/useRouteElement.tsx
+++ src/useRouteElement.tsx
@@ -1,32 +1,71 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
+import Profile from './components/Profile'
+import MainLayout from './layouts/MainLayout'
 import RegisterLayout from './layouts/RegisterLayout'
 import Login from './pages/Login'
 import ProductList from './pages/ProductList'
-import  Register  from './pages/Register'
+import Register from './pages/Register'
+const isAuth = true
+const ProtectedRoute = () => {
+  return isAuth ? <Outlet /> : <Navigate to='/login' />
+}
+const RejectedRoute = () => {
+  return !isAuth ? <Outlet /> : <Navigate to='/' />
+}
+
 export default function useRouteElement() {
   const routeElement = useRoutes([
     {
       path: '/',
       element: (
-        <RegisterLayout>
+        <MainLayout>
           <ProductList />
-        </RegisterLayout>
+        </MainLayout>
       )
     },
     {
-      path: '/login',
-      element: <RegisterLayout>
-        <Login />
-        </RegisterLayout>
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: 'profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'register',
+          element: (
+            <RegisterLayout>
           <Register />
         </RegisterLayout>
-      )
-    }
+          )
+        }
+      ]
+    },
+    
   ])
   return routeElement
 }
